1eef44c2b5313cf1f3d73fa5e5f38f886bb29df2
Philipp Gressly Freimann <phi@gressly.eu>
2014-06-20 11:39:50 +0200

    New Files due to renaming. In other words: The renaming includes the filenames.

---
+++ js/_template_.js
@@ -0,0 +1,11 @@
+// phi@gress.ly (first implementaion: April 2013)
+
+// this script is loaded at the end in "js/dom/asyncLoader.js"
+
+/**
+ * Replace the following method by whatever is defined in "js/registerHandlers.js".
+ */
+function doMy_template_Fct() {
+    var n = readNumberFromFieldViaId("myInputField");
+    outputIntoNewOrExistingParagraph("Result: " + (2*n), 'newOutputID');
+}
